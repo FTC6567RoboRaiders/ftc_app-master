13d20e5a5d34770148763ca1fe4a9464b783ab78
Derin <derinabitagaoglu@gmail.com>
2017-01-02 11:34:37 -0500

    New program ShootBallsRedLeft

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootBallsRedLeft.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by saras on 1/2/2017.
+ */
+public class ShootBallsRedLeft extends AutonomousHeader{
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+
+        calibrateGyro();
+
+        waitForStart();
+
+        Thread.sleep(10000);
+
+        shoot();
+        Thread.sleep(200);
+
+        servoGate.setPosition(0.35);
+        Thread.sleep(100);
+
+        shoot2();
+        Thread.sleep(500);
+
+        shoot3();
+        Thread.sleep(200);
+
+        gyroTurnLeft(75, 0.25);
+        Thread.sleep(200);
+
+        encodersForward(25, 0.25);
+        Thread.sleep(200);
+
+        gyroTurnLeft(35, 0.25);
+        Thread.sleep(200);
+
+        encodersForward(30, 0.25);
+        Thread.sleep(200);
+
+
+
+
+
+
+
+
+
+    }
+}
