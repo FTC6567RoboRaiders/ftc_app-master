7830e421c282f1e16d0fe2b0e7ff6e940699dbbc
Katelin Zichittella <katelinz2@frontier.com>
2017-01-29 19:15:26 -0500

    fixes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ParkBlue.java
@@ -3,11 +3,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 /**
- * Created by Katelin Zichittella on 10/26/2016.
+ * Created by Simon and Derin on 11/14/16.
  */
 
 @Autonomous
-
 public class ParkBlue extends AutonomousHeader {
 
     @Override
@@ -21,16 +20,28 @@ public class ParkBlue extends AutonomousHeader {
 
         Thread.sleep(10000);
 
-        encodersForward(4, 0.25);
+        encodersForward(6, 0.25);
         Thread.sleep(200);
 
-        gyroTurnRight(22, 0.25);
+        gyroTurnRight(20, 0.25);
         Thread.sleep(200);
 
-        encodersForward(55, 0.25);
+        encodersForward(3, 0.25);
         Thread.sleep(200);
 
-        encodersBackward(10, 0.25);
+        encodersForward(5, 0.25);
+        Thread.sleep(200);
+
+        gyroTurnRight(28, 0.25);
+        Thread.sleep(200);
+
+        encodersForward(50, 0.25);
+        Thread.sleep(200);
+
+        gyroTurnRight(30, 0.25);
+        Thread.sleep(200);
+
+        encodersForward(15, 0.25);
         Thread.sleep(200);
     }
 }
